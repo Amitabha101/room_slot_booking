14e4eefd44458e4a801fa854db7a2576694cac1d
FaLLeN <amitabhamajumder2@gmail.com>
2020-02-05 17:47:59 +0530

    added description on login_app/urls

--- room/login_app/urls.py
+++ room/login_app/urls.py
@@ -2,7 +2,12 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
+  # searches for login function in views 
   path('login',views.login,name='login'),
+
+  # searches for register function in views 
   path('register',views.register,name='register'),
+
+  # searches for logout function in views 
   path('logout',views.logout,name='logout'),
 ]
